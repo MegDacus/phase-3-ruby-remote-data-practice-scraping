c24440e585d6d1071400f4739f9ccded7f670bc1
Megan Dacus <meganjustice0609@gmail.com>
2023-05-29 14:46:01 +1000

    Assignment Completed

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(URI.open("https://flatironschool.com/"))
+courses = doc.css(".heading-25-extrabold.color-black")
+p courses[0].children
+
+# courses.each do |course|
+#     puts course.text.strip
+# end
